dfa4f455096e08638e2ef48031a09ef67c52c0e1
Shruthi Vardhini <shruthivardhini.s@gmail.com>
2024-11-27 23:23:29 +0000

    Structure2 Commit and Push

---
+++ src/test/java/TestsRunnerFile.java
@@ -0,0 +1,12 @@
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "."
+        ,plugin = { "pretty", "html:reports.html" }
+)
+
+public class TestsRunnerFile {
+
+}
